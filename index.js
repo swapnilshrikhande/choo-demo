7834af29060c92f39390d63e2fed5d0c7387133c
Glitch (curious-pepperberry) <none>
2019-10-13 06:17:25 +0000

    🏙🐟 Add state management

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ var main = require('./templates/main')
 // initialize choo
 var app = choo()
 
+//state management
+app.use(function(state){
+  state.animals = {type:'lion',x:13,y:15};
+});
+
 // create a route
 app.route('/', main)
 
