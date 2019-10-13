19fa7585acc9ab40ec51d131e3ada366e2bb0634
Glitch (curious-pepperberry) <none>
2019-10-13 06:58:04 +0000

    💏🍮 Add state mutation and event support

--- index.js
+++ index.js
@@ -8,12 +8,21 @@ var main = require('./templates/main')
 var app = choo()
 
 //state management
-app.use(function(state){
+app.use(function(state,emitter){
     // initialize state
   state.animals = [
     {type: 'lion', x: 200, y: 100},
     {type: 'crocodile', x: 50, y: 300}
   ];
+  
+  //emitter a way to signify change in the state
+  // add animal
+  emitter.on('addAnimal', function () {
+    var obj = {type: 'lion', x: 100, y: 200}
+    state.animals.push(obj)
+    emitter.emit('render')
+  })
+  
 });
 
 // create a route
--- templates/main.js
+++ templates/main.js
@@ -3,14 +3,20 @@ var html = require('choo/html')
 var animal = require('./animal')
 
 // export module
-module.exports = function (state) {
+module.exports = function (state,emit) {
   // create html template
   return html`
     <div class="container">
-      <div class="grass">
+      <div class="grass" onclick="${add}">
         <img src="/assets/bg.gif" />
         ${state.animals.map(animal)}
       </div>
     </div>
   `
+  
+  
+  //add function to add an animal to ui
+  function add () {
+    emit('addAnimal')
+  }
 }
